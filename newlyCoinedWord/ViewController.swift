72d874632d2290724904543cb8e2f20fc60e71a7
jonghwan3 <pjhan93@hanyang.ac.kr>
2022-07-09 19:09:42 +0900

    Initial Commit

---
+++ newlyCoinedWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newlyCoinedWord
+//
+//  Created by 박종환 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
